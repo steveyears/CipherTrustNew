0dcdf47d2f606721bb01b9d4bb9c8a16bd939b0d
steveyears <syears@me.com>
2024-08-05 14:07:36 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   output: 'export',
+  distDir: 'dist',
   assetPrefix: isProd ? '/CipherTrustNew/' : '',
   basePath: isProd ? '/CipherTrustNew' : '',
   images: {
