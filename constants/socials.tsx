3abcaeaaa2f7dee6b92cec271cc2c8859ef6130d
steveyears <syears@me.com>
2024-08-05 06:03:01 -0700

    Updated Social Icons

--- constants/socials.tsx
+++ constants/socials.tsx
@@ -3,19 +3,19 @@ export const socials = [
     {
         id: "0",
         title: "Twitter",
-        iconUrl: "/images/socials/twitter.svg",
+        iconUrl: "/images/socials/x2.svg",
         url: "https://x.com/thalescloudsec",
     },
     {
         id: "1",
         title: "Instagram",
-        iconUrl: "/images/socials/instagram.svg",
+        iconUrl: "/images/socials/instagram2.svg",
         url: "https://www.instagram.com/thalesgroup",
     },
     {
         id: "2",
         title: "Facebook",
-        iconUrl: "/images/socials/facebook.svg",
+        iconUrl: "/images/socials/facebook2.svg",
         url: "https://www.facebook.com/ThalesCloudSec/",
     },
 ];
