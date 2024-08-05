cb60b5f19eda6e41e623735d5cf9f652a9a1a452
steveyears <syears@me.com>
2024-08-05 11:36:56 -0700

    Changes to prepare for publishing to GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = nextConfig
+module.exports = {
+  output: 'export',
+  assetPrefix: isProd ? '/ciphertrust/' : '',
+  basePath: isProd ? '/ciphertrust' : '',
+  images: {
+    unoptimized: true,
+  },
+};
